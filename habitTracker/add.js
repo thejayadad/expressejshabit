cdc4bfee476017f5acb56717264ecb54e94e61a6
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-10-14 10:32:16 -0400

    update

--- habitTracker/add.js
+++ habitTracker/add.js
@@ -1,35 +1,120 @@
 
-const express = require("express");
+
+//Adding data into MongoDB with Express
+
+const express = require("express")
 const app = express();
 const path = require("path")
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+//Build Schema for data being added in database
+
 const Habit = require("./models/habits")
 
+// added a dotenv to protect sensitive information
 const port = process.env.PORT || 4500;
+require("dotenv").config();
 
-
-require('dotenv').config();
-
+// express to Mongodb. Add variable for Mongodb connection string
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true }
-);
+mongoose.connect(uri, { useNewUrlParser: true}
+    );
+
 const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
+connection.once("open", () => {
+    console.log("MongoDb is connected baby!")
 })
 
-const stuff = new Habit ({
-    habit: "weight lifting",
-    duration: 35,
-    category: "fitness"
-})
-stuff.save().then(stuff => {
-    console.log(stuff)
-})
-.catch(e => {
-    console.log(e);
+
+
+//How to pull items from DB and display via ejs
+
+// setting up assests in public folder
+app.use(express.static(path.join(__dirname, "public")))
+
+
+// setting up views engine
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "/views"))
+
+//middleware to allow the posting of frontend info
+app.use(express.urlencoded({extended: true}))
+
+
+
+// first get route to display data from DB thru ejs template
+
+app.get("/", async (req, res) => {
+    const habits = await Habit.find({})
+    res.render("home", {habits})
 })
 
+
+
+// Single Habit details page
+app.get("/habit/:id", async(req, res) => {
+    const {id} = req.params;
+    const habit = await Habit.findById(id)
+    res.render("show", {habit})
+})
+
+
+//get route to display new habit form
+
+
+app.get("/new", (req, res) => {
+    res.render("new");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// add a few habits to database
+
+// const stuff = new Habit({
+//     habit: "learning Javascript",
+//     duration: 60,
+//     category: "study"
+// })
+// stuff.save().then(stuff => {
+//     console.log(stuff)
+// })
+// .catch(e => {
+//     console.log(e)
+// })
+
+// listening for server on port via express app
 app.listen(5000, () => {
-    console.log("We are going on the port")
+    console.log("We are up and going")
 })
--- habitTracker/models/habits.js
+++ habitTracker/models/habits.js
@@ -1,12 +1,14 @@
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const habitSchema = new mongoose.Schema({
-    habit: { type: String, required: true },
-    duration: { type: Number, required: true },
-    category: {type: String, enum: ["study", "health", "fitness"]}
+    habit: {type: String, required: true},
+    duration: {type: Number, required: true},
+    category: {type: String, enum:["study", "fitness", "health"]}
 })
 
-const Habit = mongoose.model("Habit", habitSchema);
+const Habit = mongoose.model("Habit", habitSchema)
 
-module.exports = Habit;
+module.exports = Habit
+
+//Add into main file
